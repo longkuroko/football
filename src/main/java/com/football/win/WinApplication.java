402607d05077360260b5afe5fcc87ae841c9d721
longkuroko <longsteve.jobs@gmail.com>
2024-05-21 21:47:23 +0700

    init repository

---
+++ src/main/java/com/football/win/WinApplication.java
@@ -0,0 +1,13 @@
+package com.football.win;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinApplication.class, args);
+	}
+
+}
